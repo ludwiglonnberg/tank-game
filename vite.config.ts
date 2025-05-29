b97b1387b0c047e980c096f08cffd1edba20ef98
ludwiglonnberg <130597326+ludwiglonnberg@users.noreply.github.com>
2025-05-29 10:08:44 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
 
+// Fix för __dirname i ES-moduler
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, "..");
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: "public/index.html",
-        register: "public/register.html",
+        register: resolve(__dirname, "public/register.html"),
         home: "public/home.html",
         game: "public/game.html",
       },
